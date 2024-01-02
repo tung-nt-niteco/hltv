bacb32b6b05d7105c29e815a677887972fb3083c
tung.nt <tung.nt@CL-LT-PC2CR160>
2024-01-02 20:42:10 +0700

    Add content for Children’s Online Privacy Protection Act, and Last Updated date

--- app/page.js
+++ app/page.js
@@ -39,7 +39,9 @@ export default function Home() {
           <h2>
             Our commitment <span>-&gt;</span>
           </h2>
-          <p>When you use our services, you are trusting us with your information. We understand this is a big responsibility and work hard to protect your information and put you in control.</p>
+          <p>When you use our services (our applications), you are trusting us with your information. We understand this is a big responsibility and work hard to protect your information and put you in control.
+            <br/>Our Privacy Policy describes the information collected through our applications and the use of the information. Last updated: Jan 1st, 2024.
+          </p>
         </a>
 
         <a
@@ -61,9 +63,10 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Your rights <span>-&gt;</span>
+            Children’s privacy protection <span>-&gt;</span>
           </h2>
-          <p>No sensitive personal data is collected, but you may have the legal rights under the applicable data protection laws in relation to your data, and you can exercise these rights free of charge, by contacting us at HighLevelTechValley(@gmail.com).</p>
+          <p>We comply with the Children’s Online Privacy Protection Act (“COPPA”). We <b>do not collect personal information from children</b> under the age of 13.
+            <br/>We firmly commit that if in the event that a user identifies himself or herself as a child under the age of 13 through a support request or through any feedback, we will not collect, store or use, and will delete in a secure manner, any personal information of such user.</p>
         </a>
 
         <a
@@ -73,10 +76,10 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            We care <span>-&gt;</span>
+            Your rights <span>-&gt;</span>
           </h2>
-          <p>
-            We will respond to any rights that you exercise within a month of receiving the request unless the request is particularly complex, in which case we will respond within three months.
+          <p>No sensitive personal data is collected, but you may have the legal rights under the applicable data protection laws in relation to your data, and you can exercise these rights free of charge, by contacting us at HighLevelTechValley(@gmail.com).
+            <br/>We will respond within a month of receiving the request unless the request is particularly complex, in which case we will respond within three months.
           </p>
         </a>
       </div>
